5e0918cb9b5a7074205107c093738f4fda959734
Arsene07 <arsene07@hotmail.com>
2017-10-28 16:55:53 +0200

    libs && todos

--- iki-team/build.sh
+++ iki-team/build.sh
@@ -1,14 +1,14 @@
 echo 'compiling module'
-javac -d classes --module-path ../mods --source-path src $(find . -name '*.java')
+javac -d classes --module-path ../modules --source-path src $(find . -name '*.java')
 
 
 echo 'making module jar'
-jar --create  --file ../mods/iki-team.jar -C classes/ .
+jar --create  --file ../modules/iki-team.jar -C classes/ .
 
 
 echo 'Launching module jar'
-java --module-path ../mods/ -m iki.team/fr.zenika.iki.team.service.TeamManager
+java --module-path ../modules/ -m iki.team/fr.zenika.iki.team.service.TeamManager
 
-jdeps --module-path ../mods/  ../mods/iki-team.jar
+jdeps --module-path ../modules/  ../modules/iki-team.jar
 
-jdeps --module-path ../mods/ --jdk-internals  ../mods/iki-team.jar
+jdeps --module-path ../modules/ --jdk-internals  ../mods/iki-team.jar
