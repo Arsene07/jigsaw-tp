74bbda6e75c65d17606b93eefd67e2d3e473c218
Arsene07 <arsene07@hotmail.com>
2017-10-29 23:30:39 +0100

    typo in module name

--- iki-recruitment/build.sh
+++ iki-recruitment/build.sh
@@ -1,10 +1,10 @@
 echo 'compiling module'
-javac -d classes --module-path ../mods --source-path src $(find . -name '*.java')
+javac -d classes --module-path ../modules --source-path src $(find . -name '*.java')
 
 
 echo 'making module jar'
-jar --create  --file ../mods/iki-recruitment.jar -C classes/ .
+jar --create  --file ../modules/iki-recruitment.jar -C classes/ .
 
 
 echo 'Launching module jar'
-java --module-path ../mods/ -m iki.recruitement/fr.zenika.iki.recruitment.service.RecruitementManager
+java --module-path ../modules/ -m iki.recruitment/fr.zenika.iki.recruitment.service.RecruitementManager
