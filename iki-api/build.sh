239a19b2a622c99389e88b0c6c25eee3394f11e0
MOHAMMED ELGHAOUAT <arsene07@hotmail.com>
2017-10-30 22:33:36 +0100

    Update build.sh

--- iki-api/build.sh
+++ iki-api/build.sh
@@ -1,7 +1,7 @@
 echo 'compiling module'
 #javac -d classes --module-path ../modules:../jackson-source/modules --source-path src $(find . -name '*.java')
 
-javac -d classes --module-path ../modules:../jackson-source/modules --source-path src $(find . -name '*.java')
+javac -d classes --module-path ../modules --source-path src $(find . -name '*.java')
 
 
 
@@ -11,4 +11,4 @@ jar --create  --file ../modules/iki-api.jar -C classes/ .
 
 echo 'Launching module jar'
 #java --module-path ../modules/ --class-path ../lib/jackson-core-2.8.10.jar:../lib/jackson-annotations-2.8.10.jar -m iki.api/fr.zenika.iki.api.controller.IkiAPI
-java --module-path ../modules/:../jackson-source/modules  -m iki.api/fr.zenika.iki.api.controller.IkiAPI
+java --module-path ../modules/  -m iki.api/fr.zenika.iki.api.controller.IkiAPI
